9daa6c27f459d046e1822a80e5d4e6f50170773d
Kavya-24 <Kavya_1901cs30@iitp.ac.in>
2021-05-04 11:50:11 +0530

    Init

---
+++ Code/Project/Project.ino
@@ -0,0 +1,119 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <Wire.h>
+#include "MQ135.h"
+
+#define BLYNK_PRINT Serial
+#include <BlynkSimpleEsp8266.h>
+  
+char authCode[] = "p68yxtQRn2TfmOqlwbUZvcUPdLDQH-0L";         //Auth code for BLYNK
+char ssid[] = "Vinod";                                        //Wifi Password
+char pass[] = "21081951";                                     //Password
+
+
+
+//Constants used
+const int bitRate = 115200;
+const int gasThreshold = 190;                                 //Threshold Value
+
+int relay = 13;
+
+//Sensor
+MQ135 gasSensor = MQ135(A0);
+
+    
+//Buzzer
+int Buzzer = 8;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  Serial.begin(bitRate);
+  delay(10);
+
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, HIGH);
+  
+  Blynk.config(authCode); 
+        
+}
+
+
+void loop() {
+
+     delay(6000);    
+
+     Serial.println("In Loop");
+     
+     float h = analogRead(A0);
+     float ppm = gasSensor.getPPM();
+
+     if(isnan(h)){
+      Serial.println("Failed to read data from the sensor");
+      return;
+     }
+     
+    //H values
+    Serial.println(" ");
+    Serial.print(h);
+    
+
+    //Detect Gas
+    int conductivity = round((h/1023)*100);
+
+    //String status and state
+    String gasStatus = "Normal";
+    int gasState = 0;
+
+        
+    if(h <=190){
+      gasStatus = "Normal";
+      gasState = 0; 
+    }
+
+    else if(h <=250){
+      gasStatus = "Critical";
+      gasState = 1;
+    }
+
+
+    else{
+      
+      gasStatus = "Dangerous";
+      gasState = 1;
+      
+    }
+
+
+    if(gasState == 1){
+        
+         digitalWrite(relay, LOW);
+         tone(14,300,500);
+         Serial.println("Firing message");
+         String statusToSend = "Gas Levels is " + gasStatus + "\n" + "PPM Level is " + String(ppm);
+         Serial.println(statusToSend);
+         Blynk.connectWiFi(ssid,pass);
+          
+//         while(!Blynk.connected()){
+//            Serial.println("Connecting..");
+//            delay(500);             //Check every 0.5 seconds
+//         }
+         
+          Blynk.run();
+          Blynk.notify(statusToSend);
+          Serial.println("Message sent");
+         
+
+        delay(5000);
+    }
+    
+    
+    
+
+    //Add buzzer for 9
+    digitalWrite(relay, HIGH);
+
+    
+  
+}
